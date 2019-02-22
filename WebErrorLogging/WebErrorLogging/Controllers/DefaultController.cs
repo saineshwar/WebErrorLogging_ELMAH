3b4832e56e7c55efe37171d6f3de1fa717cd1985
saineshwar bageri <saihacksoft@gmail.com>
2019-02-23 00:19:39 +0530

    First Commit
    First Commit

---
+++ WebErrorLogging/WebErrorLogging/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebErrorLogging.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Index()
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return View();
+        }
+    }
+}
